cf740a906e45fb0096202b63897343fd59790a5b
altitudetony <tony@lunar-rover.co>
2024-08-16 01:01:48 +0800

    add init styles.js

---
+++ src/misc/styles.js
@@ -0,0 +1,98 @@
+export const theme = {
+  light: {
+    // background
+    background: {
+      primary: "#ffffff",
+      cardBg: "#EDEDED",
+      gradient: ["#7253E8", "#914BAA"],
+      onCardBg: "#ffffff",
+    },
+    // button
+    button: {
+      activeBg: "#000000",
+      activeIcon: "#ffffff",
+      activeLabel: "#ffffff",
+      activeHighlightBg: ["#7253E8", "#914BAA"],
+      inactiveBg: "#D3D3D3",
+      inactiveLabel: "#A4A4A4",
+    },
+    // text
+    text: {
+      primary: "#000000",
+      secondary: "#6B6B6B",
+    },
+  },
+  dark: {
+    // background
+    background: {
+      primary: "#24283F",
+      cardBg: "#0D1025",
+      gradient: ["#7253E8", "#914BAA"],
+      onCardBg: "#867FA1",
+    },
+    // button
+    button: {
+      activeBg: "#24283F",
+      activeIcon: "#867FA1",
+      activeLabel: "#ffffff",
+      activeHighlightBg: ["#7253E8", "#914BAA"],
+      inactiveBg: "#867FA1",
+      inactiveLabel: "#ffffff",
+    },
+    // text
+    text: {
+      primary: "#ffffff",
+      secondary: "#867FA1",
+    },
+  },
+};
+
+export const typography = {
+  fontFamily: "avenir",
+  styles: {
+    largeTitle: {
+      weight: "bold",
+      fontSize: 34,
+    },
+    title1: {
+      weight: "regular",
+      fontSize: 28,
+    },
+    title2: {
+      weight: "regular",
+      fontSize: 22,
+    },
+    title3: {
+      weight: "regular",
+      fontSize: 20,
+    },
+    headline: {
+      weight: "semibold",
+      fontSize: 17,
+    },
+    body: {
+      weight: "regular",
+      size: 17,
+    },
+    callout: {
+      weight: "regular",
+      size: 16,
+    },
+    subheading: {
+      weight: "regular",
+      size: 15,
+    },
+    footnote: {
+      weight: "regular",
+      size: 13,
+    },
+    caption1: {
+      weight: "regular",
+      size: 12,
+    },
+    caption2: {
+      weight: "regular",
+      size: 11,
+    },
+  },
+};
